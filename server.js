a0d016161103564cb8c3295fcae6ac77fab01e7d
Roger Terrill <rchicasterill@gmail.com>
2018-09-26 15:48:19 -0700

    Route Files with Express Router, just wireframed out what our routes will look like

--- server.js
+++ server.js
@@ -1,6 +1,10 @@
 const express = require('express');
 const mongoose = require('mongoose');
 
+const users = require('./routes/api/users');
+const profile = require('./routes/api/profile');
+const posts = require('./routes/api/posts');
+
 const app = express();
 
 // DB Config
@@ -13,6 +17,11 @@ mongoose
     .catch(err => console.log(err));
 
 app.get('/', (request, response) => response.send('Hello1'));
+
+// Use Routes
+app.use('/api/users', users);
+app.use('/api/profile', profile);
+app.use('/api/posts', posts);
 
 // process.env.Port is for Heroku and 5000 is for localhost
 const port = process.env.PORT || 5000;
