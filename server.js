a151ca2b7f71cbb0f09e181967f15d734c6e57d2
Roger Dodger <lpaddikt@gmail.com>
2018-09-25 23:02:29 -0700

    Comment on const port

--- server.js
+++ server.js
@@ -2,8 +2,9 @@ const express = require('express');
 
 const app = express();
 
-app.get('/', (request, response) => response.send('Hello'));
+app.get('/', (request, response) => response.send('Hello1'));
 
+// process.env.Port is for Heroku and 5000 is for localhost
 const port = process.env.PORT || 5000;
 
 app.listen(port, () => console.log(`Server running on port ${port}`));
