7ac250a1550e69360d3150d2f1b6e9b35e3b9625
Roger Terrill <rchicasterill@gmail.com>
2018-09-27 10:41:47 -0700

    User Registration: Added Gravatar and setup register post to check for existing email.

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,5 +1,10 @@
 const express = require("express");
 const router = express.Router();
+const gravatar = require('gravatar');
+const bcrypt = require('bcryptjs');
+
+// Load User model
+const User = require('../../models/User');
 
 // @route   GET api/users/test
 // @desc    Tests users route
@@ -7,5 +12,30 @@ const router = express.Router();
 router.get("/test", (request, response) =>
   response.json({ msg: "Users Works!" })
 );
+
+// @route   POST api/users/register
+// @desc    Register user
+// @access  Public
+router.post('/register', (request, response) => {
+    User.findOne({ email: request.body.email })
+        .then(user => {
+            if(user) {
+                return response.status(400).json({email: 'Email already exists'});
+            } else {
+                const avatar = gravatar.url(request.body.email, {
+                    s: '200', // Size
+                    r: 'pg', // Rating
+                    d: 'mm' // Default
+                });
+
+                const newUser = User({
+                    name: request.body.name,
+                    email: request.body.email,
+                    avatar: avatar,
+                    password: request.body.password
+                });
+            }
+        })
+});
 
 module.exports = router;
--- server.js
+++ server.js
@@ -1,27 +1,35 @@
-const express = require("express");
-const mongoose = require("mongoose");
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
 
-const users = require("./routes/api/users");
-const profile = require("./routes/api/profile");
-const posts = require("./routes/api/posts");
+const users = require('./routes/api/users');
+const profile = require('./routes/api/profile');
+const posts = require('./routes/api/posts');
 
 const app = express();
 
+// Body parser middleware
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
 // DB Config
-const db = require("./config/keys").mongoURI;
+const db = require('./config/keys').mongoURI;
 
 // Connect to MongoDB with Mongoose
 mongoose
-  .connect(db,{ useNewUrlParser: true })
-  .then(() => console.log("MongoDB Connected"))
+  .connect(
+    db,
+    { useNewUrlParser: true }
+  )
+  .then(() => console.log('MongoDB Connected'))
   .catch(err => console.log(err));
 
-app.get("/", (request, response) => response.send("Hello1"));
+app.get('/', (request, response) => response.send('Hello1'));
 
 // Use Routes
-app.use("/api/users", users);
-app.use("/api/profile", profile);
-app.use("/api/posts", posts);
+app.use('/api/users', users);
+app.use('/api/profile', profile);
+app.use('/api/posts', posts);
 
 // process.env.Port is for Heroku and 5000 is for localhost
 const port = process.env.PORT || 5000;
