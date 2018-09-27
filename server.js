c1c49972cc59ef37b48112bc2418d8fedfa87eee
Roger Terrill <rchicasterill@gmail.com>
2018-09-27 08:31:48 -0700

    Creating the User Model

--- server.js
+++ server.js
@@ -12,10 +12,7 @@ const db = require("./config/keys").mongoURI;
 
 // Connect to MongoDB with Mongoose
 mongoose
-  .connect(
-    db,
-    { useNewUrlParser: true }
-  )
+  .connect(db,{ useNewUrlParser: true })
   .then(() => console.log("MongoDB Connected"))
   .catch(err => console.log(err));
 
