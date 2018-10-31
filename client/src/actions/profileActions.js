0883bb83d581632eac5d7af83d2ee08082ffebd1
Roger Terrill <rchicasterill@gmail.com>
2018-10-31 08:58:16 -0700

    Create Profile Functionality

--- client/src/actions/profileActions.js
+++ client/src/actions/profileActions.js
@@ -1,31 +1,54 @@
 import axios from 'axios';
 
-import { GET_PROFILE, PROFILE_LOADING, CLEAR_CURRENT_PROFILE } from './types';
+import {
+  GET_PROFILE,
+  PROFILE_LOADING,
+  CLEAR_CURRENT_PROFILE,
+  GET_ERRORS
+} from './types';
 
 // Get current profile
 export const getCurrentProfile = () => dispatch => {
   dispatch(setProfileLoading());
-  axios.get('/api/profile')
-    .then(response => dispatch({
-      type: GET_PROFILE,
-      payload: response.data
-    }))
-    .catch(error => dispatch ({
-      type: GET_PROFILE,
-      payload: {}
-    }));
-}
+  axios
+    .get('/api/profile')
+    .then(response =>
+      dispatch({
+        type: GET_PROFILE,
+        payload: response.data
+      })
+    )
+    .catch(error =>
+      dispatch({
+        type: GET_PROFILE,
+        payload: {}
+      })
+    );
+};
+
+// Create Profile
+export const createProfile = (profileData, history) => dispatch => {
+  axios
+    .post('/api/profile', profileData)
+    .then(result => history.push('/dashboard'))
+    .catch(error =>
+      dispatch({
+        type: GET_ERRORS,
+        payload: error.response.data
+      })
+    );
+};
 
 // Profile loading
 export const setProfileLoading = () => {
   return {
     type: PROFILE_LOADING
-  }
-}
+  };
+};
 
 // Clear loading
 export const clearCurrentProfile = () => {
   return {
     type: CLEAR_CURRENT_PROFILE
-  }
-}
+  };
+};
